838ffc92b4f00d73384940e492b10e0d5f93807b
Athos Francisco Pereira do Carmo <athos.carmo@mcmec.mcmmontagens.com.br>
2018-11-19 16:57:45 -0300

    até a aula 18

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function(){
+    $this->get('balance', 'BalanceController@index')->name('admin.balance');
+    $this->get('/', 'AdminController@index')->name('admin.home');
 });
+
+$this->get('/', 'Site\SiteController@index');
+
+Auth::routes();
